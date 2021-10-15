d7d87c0fe269a0b22b892f1d70914c7e77ea43b5
raych2 <rtongco@gmail.com>
2021-10-14 17:20:18 -0700

    Install more dependencies

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -23,6 +24,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
