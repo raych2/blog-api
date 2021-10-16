3b2a0e6c7a290b36307b9479e4b38dfe723135ca
raych2 <rtongco@gmail.com>
2021-10-15 19:09:22 -0700

    Modify User model to hash password and add validatePassword method

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Schema = mongoose.Schema;
 
@@ -6,5 +7,15 @@ const UserSchema = new Schema({
   username: { type: String, maxLength: 50, required: true },
   password: { type: String, minLength: 6, required: true },
 });
+
+UserSchema.pre('save', async function (next) {
+  const user = this;
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+UserSchema.methods.validatePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
