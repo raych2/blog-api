a28bc6a806013d58440aa68f583e3e594ca34cc0
raych2 <rtongco@gmail.com>
2021-10-24 18:29:45 -0700

    Change order of routes

--- routes/index.js
+++ routes/index.js
@@ -7,23 +7,21 @@ const post_controller = require('../controllers/postController');
 const user_controller = require('../controllers/userController');
 const comment_controller = require('../controllers/commentController');
 
-router.post('/signup', user_controller.user_signup);
-router.post('/login', user_controller.user_login);
-
 router.get('/', post_controller.index);
 router.get('/posts', post_controller.post_list);
 
-router.get('/posts/:postId', post_controller.post_detail);
+router.post('/signup', user_controller.user_signup);
+router.post('/login', user_controller.user_login);
 
 router.post(
   '/posts/create',
   passport.authenticate('jwt', { session: false }),
   post_controller.create_post
 );
-router.delete(
-  '/posts/:postId',
-  passport.authenticate('jwt', { session: false }),
-  post_controller.delete_post
+router.get('/posts/:postId', post_controller.post_detail);
+router.post(
+  '/posts/:postId/comments/addComment',
+  comment_controller.add_comment
 );
 router.put(
   '/posts/:postId',
@@ -35,8 +33,11 @@ router.put(
   passport.authenticate('jwt', { session: false }),
   post_controller.publish_post
 );
-
-router.post('/posts/:postId/comments/addComment', comment_controller.add_comment);
+router.delete(
+  '/posts/:postId',
+  passport.authenticate('jwt', { session: false }),
+  post_controller.delete_post
+);
 router.delete(
   '/posts/:postId/comments/:commentId',
   passport.authenticate('jwt', { session: false }),
